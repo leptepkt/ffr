906c385eead8817cfd6bc46c548f1ee57ebc9bd6
Anh N. Nguyen (Joe) <namanhnguyenit@gmail.com>
2018-01-17 22:38:15 +0700

    Modify fb url

--- server/service/facebook.js
+++ server/service/facebook.js
@@ -4,9 +4,9 @@ const config = require('../config/config')
 const fb = {}
 
 fb.getLongLivedToken = (accessToken, cb) => {
+  const url = `${config.fb.baseUrl}/oauth/access_token?grant_type=fb_exchange_token&client_id=${config.fb.appId}&client_secret=${config.fb.appSecret}&fb_exchange_token=${accessToken}`
   request({
-    url: `${config.fb.baseUrl}/oauth/access_token?grant_type=fb_exchange_token
-    &client_id=${config.fb.appId}&client_secret=${config.fb.appSecret}&fb_exchange_token=${accessToken}`,
+    url: url,
   }, (error, response, body) => {
     if (cb) {
       cb(error, response, body)
